b5954b2142c4c3f5ef5372f8dbb682ea3a4b06fb
LB <lbutton@riacoding.net>
2024-06-07 09:27:16 -0700

    Changes Parameterize AWS Region

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -8,51 +8,51 @@ import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
-	auth,
-	data,
-	storage,
+  auth,
+  data,
+  storage,
 })
 const fileDirectoryName = 'skillset'
 const fileName = 'setlist.csv'
+const AWS_REGION = 'us-west-2'
 
 backend.data.resources.cfnResources.cfnGraphqlApi.environmentVariables = {
-	bucketUri: `s3://${backend.storage.resources.bucket.bucketName}/${fileDirectoryName}/${fileName}`,
+  bucketUri: `s3://${backend.storage.resources.bucket.bucketName}/${fileDirectoryName}/${fileName}`,
+  bedrockArn: `arn:aws:bedrock:${AWS_REGION}::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0`,
 }
 
 backend.addOutput({
-	custom: { fileName },
+  custom: { fileName },
 })
 
 const bedrockKBDatasource = backend.data.addHttpDataSource(
-	'BedrockHTTPDS',
-	'https://bedrock-agent-runtime.us-east-1.amazonaws.com',
-	{
-		authorizationConfig: {
-			signingRegion: 'us-east-1',
-			signingServiceName: 'bedrock',
-		},
-	}
+  'BedrockHTTPDS',
+  `https://bedrock-agent-runtime.${AWS_REGION}.amazonaws.com`,
+  {
+    authorizationConfig: {
+      signingRegion: AWS_REGION,
+      signingServiceName: 'bedrock',
+    },
+  }
 )
 
 bedrockKBDatasource.grantPrincipal.addToPrincipalPolicy(
-	new PolicyStatement({
-		resources: ['*'],
-		actions: ['bedrock:RetrieveAndGenerate'],
-	})
+  new PolicyStatement({
+    resources: ['*'],
+    actions: ['bedrock:RetrieveAndGenerate'],
+  })
 )
 
 bedrockKBDatasource.grantPrincipal.addToPrincipalPolicy(
-	new PolicyStatement({
-		resources: [`${backend.storage.resources.bucket.bucketArn}/*`],
-		actions: ['s3:getObject'],
-	})
+  new PolicyStatement({
+    resources: [`${backend.storage.resources.bucket.bucketArn}/*`],
+    actions: ['s3:getObject'],
+  })
 )
 
 bedrockKBDatasource.grantPrincipal.addToPrincipalPolicy(
-	new PolicyStatement({
-		resources: [
-			'arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0',
-		],
-		actions: ['bedrock:InvokeModel'],
-	})
+  new PolicyStatement({
+    resources: [`arn:aws:bedrock:${AWS_REGION}::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0`],
+    actions: ['bedrock:InvokeModel'],
+  })
 )
